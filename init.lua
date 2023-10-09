278ee92c6243bd1461383e1cec1ecf13da7ff29a
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2023-10-09 15:01:59 +0500

    added copilot

--- init.lua
+++ init.lua
@@ -47,6 +47,9 @@ require('lazy').setup({
   -- emmet
   'mattn/emmet-vim',
 
+  --copilot
+  'github/copilot.vim',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
